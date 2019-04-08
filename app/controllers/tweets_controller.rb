03c85730ce0a8000e6386a2919f28604621b6c16
Grace Choi <oiograceoio@gmail.com>
2019-04-08 21:17:05 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -48,7 +48,7 @@ class TweetsController < ApplicationController
 
   patch '/tweets/:id' do
     if params[:content] == ""
-      redirect to '/tweets/:id/edit'
+      redirect to "/tweets/#{tweet.id}/edit"
     else
       @tweet = Tweet.find_by_id(params[:id])
       @tweet.content = params[:content]
