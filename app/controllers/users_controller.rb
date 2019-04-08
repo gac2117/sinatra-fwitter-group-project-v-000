faf75869803635a4f9081564a054f91d0b7da182
Grace Choi <oiograceoio@gmail.com>
2019-04-08 17:27:34 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    user = User.create(params[:user])
+    if user
+      session[:user_id] = user.id
+      redirect to '/twitter'
+    else
+      redirect to '/signup'
+    end
+  end
 
 end
