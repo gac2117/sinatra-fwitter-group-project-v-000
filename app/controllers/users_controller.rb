525fc9c8a41277877a9f939b0af05403613fd52a
Grace Choi <oiograceoio@gmail.com>
2019-04-08 18:01:02 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,14 @@ class UsersController < ApplicationController
     erb :'/users/login'
   end
 
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    else
+      redirect to '/login'
+    end
+  end
+
 end
