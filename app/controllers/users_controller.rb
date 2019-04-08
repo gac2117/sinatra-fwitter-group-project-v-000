1905e56ea559854370e1d4e70f6af1b373710d5f
Grace Choi <oiograceoio@gmail.com>
2019-04-08 19:53:17 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     else
       @user = User.create(params["user"])
       session[:user_id] = @user.id
+      binding.pry
       redirect to '/tweets'
     end
   end
