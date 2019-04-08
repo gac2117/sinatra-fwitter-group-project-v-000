177eb70bf86c332c228a34f0a6067e37ca6c2633
Grace Choi <oiograceoio@gmail.com>
2019-04-08 21:11:45 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -39,8 +39,11 @@ class TweetsController < ApplicationController
   end
 
   get '/tweets/:id/edit' do
-    @tweet = Tweet.find_by_id(params[:id])
-    erb :'/tweets/edit_tweet'
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/edit_tweet'
+    end
+      
   end
 
   patch '/tweets/:id' do
